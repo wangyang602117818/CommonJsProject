e6effd1bf6507fab1db6de5c2484fa4ab63c2599
wangyang <18518317472@163.com>
2016-07-25 18:32:38 +0800

    sdf

---
+++ CommonJsProject/AppConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonJsProject
+{
+    public static class AppConfig
+    {
+        /// <summary>
+        /// 应用程序根目录  AppDomain.CurrentDomain.SetupInformation.ApplicationBase
+        /// </summary>
+        public static string BasePath = AppDomain.CurrentDomain.BaseDirectory;
+        /// <summary>
+        /// 获取配置文件节点
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string GetConfig(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+    }
+}
---
+++ CommonJsProject/Controllers/VideoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommonJsProject.Controllers
+{
+    public class VideoController : Controller
+    {
+        // GET: Video
+        public ActionResult Index()
+        {
+            string file = AppConfig.BasePath + @"video\src\1\output.m3u8";
+            return File(file, "application/x-mpegURL");
+            
+        }
+        public ActionResult GetTs(int id)
+        {
+            string filename = "output" + id + ".ts";
+            string file = AppConfig.BasePath + @"video\src\1\output\" + filename;
+            return File(file, "video/MP2T");
+        }
+    }
+}
